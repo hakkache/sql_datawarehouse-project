c523a10f4824697c50278ae1e99efb6423359cfb
HAKKACHE MOHAMED <hakkache.mohamed@gmail.com>
2025-10-28 16:20:21 +0100

    Update Create_Bronze_Tables.sql

--- scripts/bronze/Create_Bronze_Tables.sql
+++ scripts/bronze/Create_Bronze_Tables.sql
@@ -31,8 +31,8 @@ CREATE TABLE bronze.crm_cust_info(
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_last_name NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 
 );
